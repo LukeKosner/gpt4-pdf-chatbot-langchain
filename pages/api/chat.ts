1843e8b337811515765a08d01aeba6ceb8dbfc75
Luke Kosner <29708877+lukekosner@users.noreply.github.com>
2023-04-05 14:23:29 -0400

    v0.0.4

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -8,6 +8,10 @@ import {
   PINECONE_NAME_SPACE,
 } from '@/utils/pinecone-config';
 
+export const config = {
+  runtime: 'edge',
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
