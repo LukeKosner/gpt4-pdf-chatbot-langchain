98a74ac6beb660639637990d447db17a6e08704a
Luke Kosner <29708877+lukekosner@users.noreply.github.com>
2023-04-09 21:52:37 -0400

    v0.1.2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # The Auschwitz Project
+
+Analyze primary sources from Auschwitz with AI. This project is a work in progress.
+
+### Disclaimer
+
+This project is not affiliated with the Auschwitz Memorial and Museum, USHMM, or David Boder. The project is not intended to be used for research or academic purposes without fact-checking. For a definitive story of Auschwitz and the Holocaust, please visit the websites of [the Auschwitz Memorial and Museum](https://www.auschwitz.org/) and [the United States Holocaust Memorial Museum](https://ushmm.org/). Please be aware that the content of the AI generated prompts may be disturbing.
+
+### Acknowledgements
+
+- Technology made possible by @mayooear/gpt4-pdf-chatbot-langchain, GPT-4, and @hwchase17/langchainjs.
+- Powered and informed by the interviews of [David Boder](https://voices.library.iit.edu/david_boder), [the United States Holocaust Memorial Museum's oral history collection](https://collections.ushmm.org/search/catalog/irn504457), as well as [Fordham University's Internet History Sourcebooks Project](https://sourcebooks.fordham.edu/index.asp).
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import winegeart from '../public/karsten-winegeart-JioCsloIYro-unsplash.jpg';
+import murphy from '../public/colin-c-murphy-TbjTy1Q0ULM-unsplash.jpg';
+import wallace from '../public/frederick-wallace-_sVnHTFHQDU-unsplash.jpg';
+
 export default function Home() {
   return (
     <div className="bg-gray-800 text-white max-w-screen">
       <div className="flex flex-col lg:p-10 lg:pt-10 py-5 px-5 space-y-5 place-items-center text-center ">
         <p className="md:text-lg text-md">
-          Auschwitz was a place of unimaginable suffering.
+          Auschwitz was a place of unimaginable terror.
         </p>
         <div className="relative md:w-[660px] md:h-[440px] w-[330px] h-[220px]">
           <Image
-            src={
-              'https://images.unsplash.com/photo-1600356604120-a282718b29b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80'
-            }
+            src={winegeart}
             fill={true}
             alt="The train tracks leading to Auschwitz."
             className="rounded-xl"
@@ -29,9 +31,7 @@ export default function Home() {
         </p>
         <div className="relative md:w-[660px] md:h-[440px] w-[330px] h-[220px]">
           <Image
-            src={
-              'https://images.unsplash.com/photo-1638288826688-f0efcb9da93a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80'
-            }
+            src={murphy}
             fill={true}
             alt="Auschwitz interior."
             className="rounded-xl"
@@ -44,11 +44,11 @@ export default function Home() {
         </div>
         <p className="md:text-lg text-md">
           Now, we can use artificial intelligence to learn about the world's
-          greatest charnel house solely from primary accounts.
+          greatest death camp solely from primary accounts.
         </p>
         <div className="relative md:w-[660px] md:h-[440px] w-[330px] h-[220px]">
           <Image
-            src={"https://images.unsplash.com/photo-1610336875937-dcea9a658408?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"}
+            src={wallace}
             fill={true}
             alt="Auschwitz gates."
             className="rounded-xl"
